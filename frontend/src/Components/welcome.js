8bbef875628c7da2d46d3bb9d7a009901c0fc899
gamerslouis <gamerslouis@gmail.com>
2020-09-05 21:38:29 +0800

    update welcome message

--- frontend/src/Components/welcome.js
+++ frontend/src/Components/welcome.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, FormControlLabel, Checkbox, Button } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, FormControlLabel, Checkbox, Button, Typography } from '@material-ui/core';
 
 export default (props) => {
     let defaultShow
     //Check localStorage suppert
     if (window.localStorage) {
         //HACK 暫時先直接把更新時間寫死在檔案內
-        const last_update_time = 1599231176190
+        const last_update_time = 1599307893019
         if (Number(window.localStorage.getItem('hideWelcomeTime')) < last_update_time) {
             window.localStorage.setItem('hideWelcome', "")
         }
@@ -32,7 +32,12 @@ export default (props) => {
                         <li>GPA 計算機</li>
                     </ul>
                     感謝您的使用，也歡迎推廣給更多需要的朋友們。<br />
-                    若使用有任何問題或建議，請點選右下角"意見回饋"按鈕回報。
+                    若使用有任何問題或建議，請點選右下角"意見回饋"按鈕回報。<br /><br />
+                    <b>更新(2020/09/05):</b>
+                    <ul>
+                        <li>儲存模擬排課的設定</li>
+                        <li>修復新用戶無法登入的漏洞</li>
+                    </ul>
                 </DialogContentText>
                 <DialogActions>
                     {window.localStorage &&
