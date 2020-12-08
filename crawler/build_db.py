b3beaf623e4efa8a0ea57d9647db068b0896e684
gamerslouis <gamerslouis@gmail.com>
2020-12-08 12:29:45 +0800

    update crawler for new time rule

--- crawler/build_db.py
+++ crawler/build_db.py
@@ -5,6 +5,25 @@ import csv
 import code
 
 
+class _NewOldTimeConvert:
+    new_keys = 'MTWRFSUyz1234n56789abcd'
+    oringin_keys = '1234567MNABCDXEFGHYIJKL'
+
+    def __init__(self):
+        mapp = {}
+        for _ in range(len(self.new_keys)):
+            mapp[self.new_keys[_]] = self.oringin_keys[_]
+        self.mapp = mapp
+
+    def clean_time(self, time_str):
+        each_time = list(map(lambda s: s.split('-'), time_str.split(',')))
+        return ','.join([f'{self.time_map_to_origin(t)}-{str(r[0]) if len(r) > 0 else ""}' for t, *r in each_time])
+
+    def time_map_to_origin(self, time_str):
+        return ''.join(list(map(lambda c: self.mapp[c], time_str)))
+
+clean_new_time = _NewOldTimeConvert().clean_time
+
 def load_json(root, fname):
     with open(os.path.join(root, fname), 'r', encoding="utf-8") as f:
         data = json.load(f)
@@ -28,6 +47,7 @@ def work(sem, root):
         c['cos_cname'] = c['cos_cname'].replace("（", "(").replace("）", ")")
         c['cos_hours'] = str(float(c['cos_hours']))
         c['cos_credit'] = str(float(c['cos_credit']))
+        c['cos_time'] = clean_new_time(c['cos_time'])
         del c['cos_type_e']
         del c['cos_ename']
 
