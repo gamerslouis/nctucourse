76ca7516567f569ad7eb72f211d7dcaaf360c8d2
hm-ysjiang <hmysjiang.git@gmail.com>
2021-05-18 19:05:44 +0800

    Fix the settings not toggling correctly

--- frontend/src/Pages/simulator/desktop.js
+++ frontend/src/Pages/simulator/desktop.js
@@ -503,6 +503,15 @@ class Desktop extends React.Component {
     }
   }
 
+  toggleFlag(flagPos, shouldBalance) {
+    const mask = 1 << (flagPos - 1)
+    this.setState({ controlFlag: (this.state.controlFlag & (~mask)) | (evt.target.checked ? mask : 0), collapseUnsavedChange: true },
+      () => {
+        if (shouldBalance && (this.state.controlFlag & 0b100))
+          this.balanceColumns()
+      })
+  }
+
   render() {
     const { classes } = this.props
     return (
@@ -703,35 +712,16 @@ class Desktop extends React.Component {
                   <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                     <div style={{ width: '240px' }}>
                       <FormControlLabel label="顯示當學期未送註冊組課程"
-                        control={<Switch size="small" checked={(this.state.controlFlag & 0b01) > 0} onChange={evt => {
-                          this.setState({ controlFlag: (this.state.controlFlag & 0b110) | (evt.target.checked ? 0b001 : 0b000), collapseUnsavedChange: true })
-                        }} />}
+                        control={<Switch size="small" checked={(this.state.controlFlag & 0b0001) > 0} onChange={evt => this.toggleFlag(1)} />}
                       />
                       <FormControlLabel label="顯示0學分必選修"
-                        control={<Switch size="small" checked={(this.state.controlFlag & 0b10) > 0} onChange={evt => {
-                          this.setState({ controlFlag: (this.state.controlFlag & 0b101) | (evt.target.checked ? 0b010 : 0b000), collapseUnsavedChange: true },
-                            () => {
-                              if ((this.state.controlFlag & 0b100))
-                                this.balanceColumns()
-                            })
-                        }} />}
+                        control={<Switch size="small" checked={(this.state.controlFlag & 0b0010) > 0} onChange={evt => this.toggleFlag(2)} />}
                       />
                       <FormControlLabel label="自動平衡每欄高度"
-                        control={<Switch size="small" checked={(this.state.controlFlag & 0b100) > 0} onChange={evt => {
-                          this.setState({ controlFlag: (this.state.controlFlag & 0b011) | (evt.target.checked ? 0b100 : 0b000), collapseUnsavedChange: true }, () => {
-                            if (this.state.controlFlag & 0b100)
-                              this.balanceColumns()
-                          })
-                        }} />}
+                        control={<Switch size="small" checked={(this.state.controlFlag & 0b0100) > 0} onChange={evt => this.toggleFlag(3)} />}
                       />
                       <FormControlLabel label="顯示課程詳細資訊"
-                        control={<Switch size="small" checked={(this.state.controlFlag & 0b1000) > 0} onChange={evt => {
-                          this.setState({ controlFlag: (this.state.controlFlag & 0b0111) | (evt.target.checked ? 0b1000 : 0b0000), collapseUnsavedChange: true },
-                            () => {
-                              if ((this.state.controlFlag & 0b100))
-                                this.balanceColumns()
-                            })
-                        }} />}
+                        control={<Switch size="small" checked={(this.state.controlFlag & 0b1000) > 0} onChange={evt => this.toggleFlag(4)} />}
                       />
                     </div>
                     <Button variant='outlined' style={{ margin: '8px' }} onClick={evt => this.tutorNext()}>播放導覽</Button>
