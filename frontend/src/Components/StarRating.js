c63949d98eec8909c1dc948c2f0e051b531aa372
hm-ysjiang <hmysjiang.git@gmail.com>
2021-05-19 14:06:37 +0800

    Add Rating Display on feedbacks

--- frontend/src/Components/StarRating.js
+++ frontend/src/Components/StarRating.js
@@ -4,29 +4,29 @@ import React from 'react'
 export const RatingEdit = ({ size, value, onChange, ...otherProps }) => {
     const [hover, setHover] = React.useState(0)
     return (
-        <div {...otherProps} style={{ cursor: 'pointer' }}>
+        <div {...otherProps} style={{ display: 'flex', flexDirection: 'row', cursor: 'pointer' }}>
             {
                 new Array(size).fill(0).map(
                     (_, idx) => (
-                        <>
+                        <div key={idx}>
                             {
                                 (hover === 0 && value === 0)
                                     ?
-                                    <StarBorder key={idx} onClick={evt => onChange(idx + 1)}
+                                    <StarBorder onClick={evt => onChange(idx + 1)}
                                         onMouseEnter={evt => setHover(idx + 1)}
                                         onMouseLeave={evt => setHover(0)} />
                                     :
                                     (idx >= (hover || value)) ?
-                                        <StarBorder key={idx} onClick={evt => onChange(idx + 1)}
+                                        <StarBorder onClick={evt => onChange(idx + 1)}
                                             onMouseEnter={evt => setHover(idx + 1)}
                                             onMouseLeave={evt => setHover(0)} />
                                         :
-                                        <Star key={idx} onClick={evt => onChange(idx + 1)}
+                                        <Star onClick={evt => onChange(idx + 1)}
                                             onMouseEnter={evt => setHover(idx + 1)}
                                             onMouseLeave={evt => setHover(0)}
                                             style={{ color: 'orange' }} />
                             }
-                        </>
+                        </div>
                     )
                 )
             }
@@ -35,31 +35,35 @@ export const RatingEdit = ({ size, value, onChange, ...otherProps }) => {
 }
 
 export const RatingDisplay = ({ size, value, ...otherProps }) => {
-    const width = Math.round(24 * value / 5)
+    const width = Math.round(24 * (value % 1))
     return (
-        <div {...otherProps}>
+        <div {...otherProps} style={{ display: 'flex', flexDirection: 'row' }}>
             {
                 new Array(size).fill(0).map(
                     (_, idx) => (
-                        <>
+                        <div key={idx}>
                             {
-                                (idx + 1 <= value)
+                                isNaN(value)
                                     ?
-                                    <Star key={idx} style={{ color: 'orange' }} />
+                                    <Star style={{ color: 'gray' }} />
                                     :
-                                    (idx > value) ?
-                                        <StarBorder key={idx} />
+                                    (idx + 1 <= value)
+                                        ?
+                                        <Star style={{ color: 'orange' }} />
                                         :
-                                        <div key={idx} style={{ position: 'relative', width: '24px', height: '24px' }}>
-                                            <div style={{ position: 'absolute', width: `${width}px`, height: '24px', top: 0, left: 0, overflow: 'hidden' }} >
-                                                <Star style={{ color: 'orange' }} />
+                                        (idx > value) ?
+                                            <StarBorder />
+                                            :
+                                            <div style={{ position: 'relative', width: '24px', height: '24px' }}>
+                                                <div style={{ position: 'absolute', width: `${width}px`, height: '24px', top: 0, left: 0, overflow: 'hidden' }} >
+                                                    <Star style={{ color: 'orange' }} />
+                                                </div>
+                                                <div style={{ position: 'absolute', width: `${24 - width}px`, height: '24px', top: 0, right: 0, overflow: 'hidden' }} >
+                                                    <StarBorder style={{ position: 'absolute', top: 0, right: 0 }} />
+                                                </div>
                                             </div>
-                                            <div style={{ position: 'absolute', width: `${24 - width}px`, height: '24px', top: 0, right: 0, overflow: 'hidden' }} >
-                                                <StarBorder style={{ position: 'absolute', top: 0, right: 0 }} />
-                                            </div>
-                                        </div>
                             }
-                        </>
+                        </div>
                     )
                 )
             }
