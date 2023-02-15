3d85f63d60322d889aa5fadd7de7c677684d3d1d
gamerslouis <gamerslouis@gmail.com>
2023-02-15 13:14:58 +0800

    Fix warnings

--- frontend/src/Pages/simulation/prettiertable/setting.js
+++ frontend/src/Pages/simulation/prettiertable/setting.js
@@ -130,18 +130,6 @@ const ExtendedCheckbox = ({ code, values, useNewCode, setValues }) => {
     );
 };
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 const CourseTypeEditor = ({
     allCourses,
     courseIds,
@@ -263,7 +251,7 @@ const Setting = ({
             if (defaultSemester) setSelectSemester(defaultSemester);
             else setSelectSemester(data[data.length - 1]);
         }
-    }, [loading, error, data]);
+    }, [loading, error, data, defaultSemester]);
 
     const handleSetScreenSizeClick = useCallback(() => {
         setTableWidth(window.screen.width);
